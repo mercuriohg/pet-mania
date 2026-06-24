e882e1fe85b6cbea4b115bc85359576a940fb5fe
Arthur Gomes <gomesarthursevero6@gmail.com>
2026-06-24 18:49:08 -0300

    mudando o toogle.js

--- src/public/assets/js/Toogle.js
+++ src/public/assets/js/Toogle.js
@@ -1,5 +1,4 @@
 const menutoggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu');
 menutoggle.addEventListener('click', function() {
-    menu.classList.toggle('active');
 });
